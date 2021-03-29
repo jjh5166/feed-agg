b1eb8066491a6eb0f41fd9d42ada4b8a1d08a782
John <jjh5166@gmail.com>
2021-03-29 22:52:22 +0100

    /api/twitter

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const twitter = require('./feeds/twitter')
 const express = require('express')
 
 const PORT = process.env.PORT || 3001
@@ -6,6 +10,13 @@ const app = express()
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' })
+})
+
+app.get('/api/twitter', async (req, res) => {
+  const tweets = await twitter.getTweets()
+  res.json({
+    data: tweets,
+  })
 })
 
 app.listen(PORT, () => {
