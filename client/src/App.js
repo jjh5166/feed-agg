e1efd2e2d56ad99b1c123c9388f3a66059345e0c
John <jjh5166@gmail.com>
2021-03-30 22:11:25 +0100

    retrieve twitter posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styled from 'styled-components'
 
-function App() {
+import FeedItem from './Components/FeedItem'
+
+const App = ({className}) => {
   const [data, setData] = React.useState(null)
 
   React.useEffect(() => {
-    fetch('/api')
+    fetch('/api/twitter')
       .then((res) => res.json())
-      .then((data) => setData(data.message))
+      .then((data) => {
+        setData(data.data)
+      })
   }, [])
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>{!data ? 'Loading...' : data}</p>
-      </header>
+    <div className={className}>
+      <div className='feed'>
+        {data && !!data.length && data.map((item, i) => <FeedItem key ={i} data={item}/>)}
+      </div>
     </div>
   )
 }
 
-export default App
+
+const StyledApp = styled(App)`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding: 25px 0;
+  div.feed {
+    width: 75%;
+  }
+`
+export default StyledApp
