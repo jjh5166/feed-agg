ac04ed9a80e3130389e4fcbaf9ea49a97be6dc97
John <jjh5166@gmail.com>
2021-03-30 19:44:56 +0100

    add source to getTweets

--- server/feeds/twitter.js
+++ server/feeds/twitter.js
@@ -3,14 +3,15 @@ const axios = require('axios')
 const url = `https://api.twitter.com/2/users/${process.env.twitterId}/tweets`
 
 const getTweets = async () => {
-  const tweets = await axios.get(
-    url,
-    { params: { 'tweet.fields': 'created_at' } ,
-     headers: { Authorization: `Bearer ${process.env.twitterBearerToken}` } }
-  )
+  const tweets = await axios
+    .get(url, {
+      params: { 'tweet.fields': 'created_at' },
+      headers: { Authorization: `Bearer ${process.env.twitterBearerToken}` },
+    })
     .then((res) => res.data)
     .catch((err) => console.log(err))
-  return tweets
+  tweets.data.forEach((el) => (el.source = 'twitter'))
+  return tweets.data
 }
 
 exports.getTweets = getTweets
