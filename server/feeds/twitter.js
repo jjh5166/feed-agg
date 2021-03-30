69ac7f0fd4a1deab6a3bf9e626154e5fa66c8bbd
John <jjh5166@gmail.com>
2021-03-30 21:58:11 +0100

    add username

--- server/feeds/twitter.js
+++ server/feeds/twitter.js
@@ -5,12 +5,12 @@ const url = `https://api.twitter.com/2/users/${process.env.twitterId}/tweets`
 const getTweets = async () => {
   const tweets = await axios
     .get(url, {
-      params: { 'tweet.fields': 'created_at' },
+      params: { 'tweet.fields': 'created_at', 'expansions': 'author_id', 'user.fields': 'username' },
       headers: { Authorization: `Bearer ${process.env.twitterBearerToken}` },
     })
     .then((res) => res.data)
     .catch((err) => console.log(err))
-  tweets.data.forEach((el) => (el.source = 'twitter'))
+  tweets.data.forEach((el) => (el.source = 'twitter', el.username = tweets.includes.users[0].username))
   return tweets.data
 }
 
