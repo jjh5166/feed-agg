dc284d35b6e7decc85ebfa7a815c5c0286881fb9
John <jjh5166@gmail.com>
2021-03-31 22:48:02 +0100

    deploy setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 const express = require('express')
 
 const twitter = require('./feeds/twitter')
@@ -9,6 +10,10 @@ const youtube = require('./feeds/youtube')
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
 
 app.get('/api/twitter', async (req, res) => {
   const tweets = await twitter.getTweets()
@@ -24,6 +29,6 @@ app.get('/api/youtube', async (req, res) => {
   })
 })
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`)
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`) // eslint-disable-line no-console
 })
