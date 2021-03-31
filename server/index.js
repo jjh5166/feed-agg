9a6d1f1e0ebceb24ff4747a4123bf96926c204fb
John <jjh5166@gmail.com>
2021-03-31 15:22:47 +0100

    youtube endpoint

--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const twitter = require('./feeds/twitter')
 const express = require('express')
+
+const twitter = require('./feeds/twitter')
+const youtube = require('./feeds/youtube')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' })
-})
-
 app.get('/api/twitter', async (req, res) => {
   const tweets = await twitter.getTweets()
   res.json({
     data: tweets,
+  })
+})
+
+app.get('/api/youtube', async (req, res) => {
+  const uploads = await youtube.getUploads()
+  res.json({
+    data: uploads,
   })
 })
 
