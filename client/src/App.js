ff3df57918451c671420be6baab54789da1add61
John <jjh5166@gmail.com>
2021-03-31 15:23:32 +0100

    api calls, preventing race condition

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 import FeedItem from './Components/FeedItem'
 
-const App = ({className}) => {
-  const [data, setData] = React.useState(null)
-
-  React.useEffect(() => {
+const App = ({ className }) => {
+  const [data, setData] = useState([])
+  const updateData = (newData) => {
+    setData((prevData) => [...prevData, ...newData])
+  }
+  useEffect(() => {
     fetch('/api/twitter')
       .then((res) => res.json())
-      .then((data) => {
-        setData(data.data)
+      .then((res) => {
+        updateData(res.data)
       })
   }, [])
+  useEffect(() => {
+    fetch('/api/youtube')
+      .then((res) => res.json())
+      .then((res) => {
+        updateData(res.data)
+      })
+  }, [])
+
+  console.log(data)
   return (
     <div className={className}>
       <div className='feed'>
-        {data && !!data.length && data.map((item, i) => <FeedItem key ={i} data={item}/>)}
+        {data &&
+          !!data.length &&
+          data.map((item, i) => <FeedItem key={i} data={item} />)}
       </div>
     </div>
   )
 }
-
 
 const StyledApp = styled(App)`
   height: 100vh;
